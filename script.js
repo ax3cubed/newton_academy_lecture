40fd7921e313085395e00e8e60ca98f34ceac594
Alawode Emmanuel <emmanuel.o.alawode@gmail.com>
2024-05-23 02:37:30 +0100

    Changes to be committed: 	modified:   index.html 	new file:   script.js     deleted:    style.css 	new file:   styles.css
    Assignment Submission.

---
+++ script.js
@@ -0,0 +1,15 @@
+//script.js
+var coll = document.getElementsByClassName("togglebutton");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var todolist = this.nextElementSibling;
+    if (todolist.style.display === "block") {
+      todolist.style.display = "none";
+    } else {
+      todolist.style.display = "block";
+    }
+  });
+}
